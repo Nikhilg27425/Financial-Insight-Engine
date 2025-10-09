0d59600ecb19ceddfa001baa0d7ea57b68cc4e6e
RandomShivam <23ucs706@lnmiit.ac.in>
2025-10-10 01:17:41 +0530

    Changed Upload Functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,7 @@
+// main.jsx: This is the application entry point. It's the first file that runs. 
+// Its job is to "mount" your App.jsx component into the index.html file. You won't touch this often.
+
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
