f23aad18660bf40ea4f7763f6c7e4d1c89eec7cc
KeshavLaddha03 <23ucs616@lnmiit.ac.in>
2025-10-15 04:07:44 +0530

    Preprocessing Data

--- backend/main.py
+++ backend/main.py
@@ -19,8 +19,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["Authorization", "Content-Type"]
+    # allow_methods=["GET", "POST", "OPTIONS"],
+    # allow_headers=["Authorization", "Content-Type"]
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(upload.router, prefix="/upload", tags=["Upload"])
